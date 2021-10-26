0b1d2265ddef56d7f6e8ece1ad0064107a1a2da7
Kat <kyevsukov@gmail.com>
2021-10-26 12:26:17 -0400

    moved adopter end points to router

---
+++ api/adopters/adopters-router.js
@@ -0,0 +1,28 @@
+const express = require('express')
+
+const router = express.Router()
+
+//endpoints here that have to do with adopters
+router.get('/api/adopters', (req, res) => {
+    
+  });
+  
+  router.get('/api/adopters/:id', (req, res) => {
+    
+  });
+  
+  router.get('/api/adopters/:id/dogs', (req, res) => {
+  
+  });
+  
+  router.post('/api/adopters', (req, res) => {
+   
+  });
+  
+  router.delete('/api/adopters/:id', (req, res) => {
+  
+  });
+  
+  router.put('/api/adopters/:id', (req, res) => {
+    
+  });
